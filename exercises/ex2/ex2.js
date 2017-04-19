a707c0fb7125e7d0d213a2790535be2d6c0a4d19
Adrian Mui <adrian.mui@gmail.com>
2017-04-19 13:12:24 -0500

    ex2, point-free style

--- exercises/ex2/ex2.js
+++ exercises/ex2/ex2.js
@@ -1,27 +1,34 @@
-function output(txt) {
-	console.log(txt);
+// skipped this due to meeting so 
+// i will just try to understand this.
+
+function not(predicate) {
+    return function negated(...args) {
+        return !predicate(...args);
+    };
 }
 
-function printIf(predicate) {
-	return function(msg) {
-		if (predicate(msg)) {
-			output(msg);
-		}
-	};
+function when(fn) {
+    return function(predicate) {
+        return function(...args) {
+            if (predicate(...args)) {
+                return fn(...args);
+            }
+        };
+    };
 }
 
 function isShortEnough(str) {
-	return str.length <= 5;
+    return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
-}
+var output = console.log.bind(console);
+var printIf = when(output);
+var isLongEnough = not(isShortEnough);
 
 var msg1 = "Hello";
 var msg2 = msg1 + " World";
 
-printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isLongEnough)(msg2); // Hello World
