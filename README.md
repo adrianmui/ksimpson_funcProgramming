6a3343392fbae1239bc00e47a7f793a7122e8704
Adrian Mui <adrian.mui@gmail.com>
2017-04-19 13:00:09 -0500

    ex1 comp

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 functional programming workshop exercises by Kyle Simpson
 
 ex*-fixed.js are written solutions
+
 ex*.js is the code that I worked on.
--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -1,12 +1,38 @@
+// interface
+
 function foo(x) {
-	y++;
-	z = x * y;
+    y++;
+    z = x * y;
 }
 
-var y = 5, z;
+function bar1(cx, cz) {
+    var [oy, oz] = [y, z];
 
-foo(20);
-z;		// 120
+    foo(cx);
+    // i don't like interfacing. I understand it but it just seems cheap to me.
+    return [y, z];
+}
 
-foo(25);
-z;		// 175
+// functional
+
+function bar2(x, y) {
+    var z;
+    foo2(x);
+    return [y, z];
+
+    function foo2(x) {
+        y++;
+        z = x * y;
+    }
+}
+
+var y = 5,
+    z;
+
+temp = bar1(20, 5);
+console.log('bar1: ', temp);
+z; // 120
+
+temp2 = bar2(25, 6);
+console.log('bar2: ', temp2);
+z; // 175
